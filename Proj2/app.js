5cb099ac85c811d6a171e199d7a34f15fa29ef99
BonnyWang <bonny.y.gardy@gmail.com>
2023-04-01 23:58:54 -0400

    Added an CMB Sphere

--- Proj2/app.js
+++ Proj2/app.js
@@ -1,214 +1,201 @@
 "use strict";
 
-function main() {
-	var canvas = document.querySelector("#canvas");
-	canvas.width = window.innerWidth;
+// This function is based on https://webglfundamentals.org/webgl/lessons/webgl-load-obj.html
+function parseOBJ(text) {
+ 
+  // because indices are base 1 so fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
+
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
+
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
+
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+    setGeometry();
+  }
+
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  return {
+    position: webglVertexData[0],
+    texcoord: webglVertexData[1],
+    normal: webglVertexData[2],
+  };
+}
+
+async function main() {
+
+
+  const canvas = document.querySelector("#canvas");
+  canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	var gl = canvas.getContext("webgl");
-	if (!gl) {
-		return;
-	}
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
 
-	var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
+  // compiles and links the shaders, looks up attribute and uniform locations
+  var programInfo = webglUtils.createProgramInfo(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
 
-	var positionLocation = gl.getAttribLocation(program, "a_position");
-	var texcoordLocation = gl.getAttribLocation(program, "a_texcoord");
+  const response = await fetch('./sphere.obj');  
+  const text = await response.text();
+  const data = parseOBJ(text);
 
-	var matrixLocation = gl.getUniformLocation(program, "u_matrix");
-	var textureLocation = gl.getUniformLocation(program, "u_texture");
+  // Because the array is matched with the atributes, this function can  be used
+  // **Need to make sure the names are matched exactly! "a_name" -> "name".
+  // gl.createBuffer, gl.bindBuffer, gl.bufferData
+  const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
 
-	var positionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const cameraTarget = [0, 0, 0];
+  const cameraPosition = [0, 0, 4];
+  const zNear = 0.1;
+  const zFar = 50;
 
-	// TODO: Change to my Geometry
-	setGeometry(gl);
-
-	var texcoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-	
-	// TODO: Change to my texture
-	setTexcoords(gl);
-
-	// For Texture
-	var texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	
-	// Fill the texture with default when the image is not loading
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-					new Uint8Array([0, 0, 255, 255]));
-	
-	// Asynchronously load an image
-	var image = new Image();
-	image.src = "./cmbpic.png";
-	image.addEventListener('load', function() {
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-		gl.generateMipmap(gl.TEXTURE_2D);
-	});
+  var textureLocation = gl.getUniformLocation(programInfo.program, "u_texture");
+  // Handle Texture
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  
+  // Fill texture with default when image not loaded
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+                new Uint8Array([0, 0, 255, 255]));
+  
+  // Asynchronously load an image
+  var image = new Image();
+  image.src = "./cmbpic.png";
+  image.addEventListener('load', function() {
+    // Now that the image has loaded make copy it to the texture.
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+  });
 
 
-	function radToDeg(r) {
-		return r * 180 / Math.PI;
-	}
+  function degToRad(deg) {
+    return deg * Math.PI / 180;
+  }
 
-	function degToRad(d) {
-		return d * Math.PI / 180;
-	}
+  function render(time) {
+    time *= 0.001;  // convert to seconds
 
-	var fieldOfViewRadians = degToRad(60);
-	var modelXRotationRadians = degToRad(0);
-	var modelYRotationRadians = degToRad(0);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
 
-	var then = 0;
-
-	requestAnimationFrame(drawScene);
-
-  // Render Cycle
-	function drawScene(now) {
-		now *= 0.001;
-		var deltaTime = now - then;
-		then = now;
-
-		// Clip space to pixels
-		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-		gl.enable(gl.CULL_FACE);
-		gl.enable(gl.DEPTH_TEST);
-
-		// Animate the rotation
-		modelXRotationRadians += 1.2 * deltaTime;
-		modelYRotationRadians += 0.7 * deltaTime;
-
-		// Clear the canvas
-		gl.clearColor(0, 0, 0, 1)
+    gl.clearColor(0, 0, 0, 1)
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-		gl.useProgram(program);
 
-		gl.enableVertexAttribArray(positionLocation);
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
 
-		var size = 3;          
-		var type = gl.FLOAT;   
-		var normalize = false; 
-		var stride = 0;        
-		var offset = 0;        
-		gl.vertexAttribPointer(
-			positionLocation, size, type, normalize, stride, offset);
+    // Make a view matrix from the camera matrix.
+    const view = m4.inverse(camera);
 
-		// For texture
-		gl.enableVertexAttribArray(texcoordLocation);
-
-		gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-
-		var size = 2;          
-		var type = gl.FLOAT;   
-		var normalize = false; 
-		var stride = 0;        
-		var offset = 0;        
-		gl.vertexAttribPointer(
-			texcoordLocation, size, type, normalize, stride, offset);
-
-		// Projection Matrix
-		var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-		var projectionMatrix =
-			m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
-
-		var cameraPosition = [0, 0, 200];
-		var up = [0, 1, 0];
-		var target = [0, 0, 0];
-
-		// Compute the camera's matrix 
-		var cameraMatrix = m4.lookAt(cameraPosition, target, up);
-
-		// Make a view matrix 
-		var viewMatrix = m4.inverse(cameraMatrix);
-
-		var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-
-		var matrix = m4.xRotate(viewProjectionMatrix, modelXRotationRadians);
-		matrix = m4.yRotate(matrix, modelYRotationRadians);
-
-
-		gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-		gl.uniform1i(textureLocation, 0);
-
-		// Draw the geometry.
-		gl.drawArrays(gl.TRIANGLES, 0, 16 * 6);
-
-		requestAnimationFrame(drawScene);
-	}
-}
-
-
-function setGeometry(gl) {
-  var positions = new Float32Array([
-          ]);
-
-
-  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-}
-
-
-function setTexcoords(gl) {
-  gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array([
-       ]),
-      gl.STATIC_DRAW);
-}
-
-// For Generating Sphere
-function sphere(density){
-  const radsPerUnit = Math.PI / density;
-  const sliceVertCount = density * 2;
-
-
-  const positions = [];
-  let latitude = -Math.PI / 2;
-  //latitude
-  for(let i = 0; i <= density; i++){
-      if(i === 0 || i === density){ //polar caps
-          positions.push(latLngToCartesian([1, latitude, 0]));
-      } else {
-          let longitude = 0;
-          for (let j = 0; j < sliceVertCount; j++) {
-              positions.push(latLngToCartesian([1, latitude, longitude]));
-              longitude += radsPerUnit;
-          }
-      }
-      latitude += radsPerUnit;
-  }
-
-  function parseOBJ(text) {
- 
-    const keywords = {
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
     };
-   
-    const keywordRE = /(\w*)(?: )*(.*)/;
-    const lines = text.split('\n');
-    for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
-      const line = lines[lineNo].trim();
-      if (line === '' || line.startsWith('#')) {
-        continue;
-      }
-      const m = keywordRE.exec(line);
-      if (!m) {
-        continue;
-      }
-      const [, keyword, unparsedArgs] = m;
-      const parts = line.split(/\s+/).slice(1);
-      const handler = keywords[keyword];
-      if (!handler) {
-        console.warn('unhandled keyword:', keyword, 'at line', lineNo + 1);
-        continue;
-      }
-      handler(parts, unparsedArgs);
-    }
-  }
-}
 
+    gl.useProgram(programInfo.program);
+
+    webglUtils.setUniforms(programInfo, sharedUniforms);
+
+    // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
+    webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+
+    // calls gl.uniform
+    webglUtils.setUniforms(programInfo, {
+      u_world: m4.yRotation(time),
+      u_diffuse: [1, 1, 0.5, 1],
+    });
+
+    // For texture mapping
+    gl.uniform1i(textureLocation, 0);
+    
+    // calls gl.drawArrays or gl.drawElements
+    webglUtils.drawBufferInfo(gl, bufferInfo);
+
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
 
 main();
